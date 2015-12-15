bb388f9570472c30ee6c414904f97fbc142f64d6
kurattila <kur.attila+github@gmail.com>
2015-12-15 19:48:23 +0100

    PlayStopButton introduced for improved UX

---
+++ PlayStopButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Cider_x64
+{
+    class PlayStopButton : CheckBox
+    {
+        static PlayStopButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PlayStopButton), new FrameworkPropertyMetadata(typeof(PlayStopButton)));
+        }
+    }
+}
